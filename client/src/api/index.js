2fa23e65239983cf545c74a35dad18a64dcc1c44
Aditya Singh <lci2023003@iiitl.ac.in>
2025-08-22 15:53:19 +0530

    Added client folder as normal files

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { getBaseURL } from "../utils/utils.js";
-const url = getBaseURL()
-const instance = axios.create({
-    baseURL: url,
-  });
- 
-export default instance
-  
